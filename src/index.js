d0e988d650aa9d128e6a6eefae73581f835b4efd
Anmol Arora <anmol.appknit@gmail.com>
2021-11-07 02:30:15 +0530

    Creating the Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+
 import "./index.css";
 import App from "./components/App";
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('Store:', store);
+console.log('State:', store.getState());
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<App />,
 	document.getElementById("root")
 );
